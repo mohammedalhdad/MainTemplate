ffcff4fd7341368acd6134f57ed3d994eabe0b62
mohammedalhdad <mohammedalhdad@hotmail.com>
2024-09-23 20:08:43 +0300

    AFter add API

---
+++ src/API/Infrastructure/SwaggerCustomSchemaFilter.cs
@@ -0,0 +1,22 @@
+﻿using NJsonSchema.Generation;
+using NJsonSchema;
+
+namespace API.Infrastructure;
+
+public class SwaggerCustomSchemaFilter : ISchemaProcessor
+{
+    public void Process(SchemaProcessorContext context)
+    {
+        // تغيير اسم الكلاس بناءً على النوع
+        if (context.Schema.Type == JsonObjectType.Object)
+        {
+            context.Schema.Title = GetCustomClassName(context.ContextualType.Type);
+        }
+    }
+
+    private string GetCustomClassName(Type type)
+    {
+        // هنا يمكنك تخصيص منطق تغيير الاسم كما ترغب
+        return type.Name.ToUpper().Replace("DTO", ""); // مثال: تغيير اسم الكلاس من DTO إلى Model
+    }
+}
