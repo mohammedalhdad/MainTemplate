a4a0bec5c405a844d24a3c8b25845564a5e39473
mohammedalhdad <mohammedalhdad@hotmail.com>
2025-03-21 14:19:27 +0300

    add EncryptionService

---
+++ src/Application/Common/Interfaces/IEncryptionService.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitecture.Application.Common.Interfaces;
+
+public interface IEncryptionService
+{
+    string Encrypt(string plainText);
+    string Decrypt(string encryptedText);
+}
---
+++ src/Infrastructure/Security/EncryptionService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace CleanArchitecture.Infrastructure.Security;
+
+public class EncryptionService : IEncryptionService
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _encryptionKey;
+
+    public EncryptionService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _encryptionKey = _configuration["EncryptionKey"] ?? throw new Exception("Encryption key is not set.");
+    }
+
+    public string Encrypt(string plainText)
+    {
+        using var aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(_encryptionKey);
+        aes.GenerateIV();
+
+        using var encryptor = aes.CreateEncryptor();
+        var plainBytes = Encoding.UTF8.GetBytes(plainText);
+        var encryptedBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+
+        var result = Convert.ToBase64String(aes.IV.Concat(encryptedBytes).ToArray());
+        return result;
+    }
+
+    public string Decrypt(string encryptedText)
+    {
+        var fullCipher = Convert.FromBase64String(encryptedText);
+        using var aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(_encryptionKey);
+
+        var iv = fullCipher.Take(16).ToArray();
+        var cipher = fullCipher.Skip(16).ToArray();
+        aes.IV = iv;
+
+        using var decryptor = aes.CreateDecryptor();
+        var plainBytes = decryptor.TransformFinalBlock(cipher, 0, cipher.Length);
+
+        return Encoding.UTF8.GetString(plainBytes);
+    }
+}
